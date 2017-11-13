793cd555e81e3c5f727a017847afbad9981dff04
Glitch (soft-plantation) <none>
2017-11-13 23:06:08 +0000

    Full integration with angular works

--- app/controllers/yelpController.client.js
+++ app/controllers/yelpController.client.js
@@ -6,157 +6,131 @@
         $interpolateProvider.endSymbol('}]}');
     });
     app.controller('yelpController', function($scope) {
-        console.log("Hello");
-        $scope.stores = [{id: "Pie"}];
-    
-    var attendButtons = [true, true, true, true, true];
-    var DIV = document.createElement("DIV");
+        $scope.stores = [];
+        var attendButtons = [true, true, true, true, true];
+        var DIV = document.createElement("DIV");
 
-    function ready(fn) {
-        if (typeof fn !== 'function') {
-            return;
-        }
-        if (document.readyState === 'complete') {
-            return fn();
-        }
-        document.addEventListener('DOMContentLoaded', fn, false);
-    }
-
-    function ajaxRequest(method, url, callback) {
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function() {
-            if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-                callback(xmlhttp.response);
+        function ready(fn) {
+            if (typeof fn !== 'function') {
+                return;
             }
-        };
-        xmlhttp.open(method, url, true);
-        xmlhttp.send();
-    }
-    var apiUrl = 'https://tabletop.glitch.me/';
-    var user;
-
-    function getUser(callback) {
-        ajaxRequest('GET', apiUrl + "users/user_data", function(data) {
-            data = JSON.parse(data);
-            if (data.hasOwnProperty('username')) {
-                user = data.username;
+            if (document.readyState === 'complete') {
+                return fn();
             }
-            callback();
-        });
-    }
-    var logged = false;
-    getUser(function() {
-        if (user != undefined) {
-            logged = true;
+            document.addEventListener('DOMContentLoaded', fn, false);
         }
-    });
 
-    function rsvp(test) {
-        ajaxRequest('GET', apiUrl + "/api/addRSVP/?user=" + user + "&id=" + test, function(data) {
- 
-        });
-    }
-
-    function remove(test) {
-        ajaxRequest('GET', apiUrl + "/api/removeRSVP/?user=" + user + "&id=" + test, function(data) {
-     
-        });
-    }
-    var searchTerm = document.getElementById("searchBar");
-    var buttonToSubmit = document.getElementById("findStores");
-    $('#findStores').submit(function(e) {
-        e.preventDefault();
-        var searchText = searchTerm.value;
-        var users = [];
-        var counter = 0;
-        ajaxRequest('GET', apiUrl + "api/yelp/?location=" + searchText, function(data) {
-            data = JSON.parse(data);
-            console.log(data);
-            /*   app.controller('yelpController', function($scope, service) {
-                   $scope.dataHasLoaded = false;
-                   service.loadData().then(function(data) {
-                       this.Stores = data;
-                       $scope.dataHasLoaded = true
-                   })
-               })  */
-            mySyncFunction(counter);
-
-            function mySyncFunction(counter) {
-                if (counter === undefined) {
-                    counter = 0;
+        function ajaxRequest(method, url, callback) {
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function() {
+                if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+                    callback(xmlhttp.response);
                 }
-                if (counter >= 5) {
-                    theRest();
-                    return;
-                };
-                ajaxRequest('GET', apiUrl + "api/getUsers/?id=" + data[counter].id, function(data) {
-                    data = JSON.parse(data);
-                    users.push(data);
-                    counter++;
-                    mySyncFunction(counter);
-                })
-            }
+            };
+            xmlhttp.open(method, url, true);
+            xmlhttp.send();
+        }
+        var apiUrl = 'https://tabletop.glitch.me/';
+        var user;
 
-            function theRest() {
-                $scope.id = [];
-                for (var i = 0; i < 5; i++) {
-                   $scope.$apply(function () {
-                     $scope.stores.push({id: data[i].id});
+        function getUser(callback) {
+            ajaxRequest('GET', apiUrl + "users/user_data", function(data) {
+                data = JSON.parse(data);
+                if (data.hasOwnProperty('username')) {
+                    user = data.username;
+                }
+                callback();
+            });
+        }
+        var logged = false;
+        getUser(function() {
+            if (user != undefined) {
+                logged = true;
+            }
         });
-       
-                    console.log($scope.stores);
-                  
-                    console.log(data[i]);
-                    DIV.className = "yelpContainer";
-                    document.getElementById("display").appendChild(DIV);
-                    var A = document.createElement("A");
-                    var img = document.createElement("img");
-                    img.src = data[i].image;
-                    A.appendChild(img);
-                    var h2 = document.createElement("h2");
-                    var textNode = document.createTextNode(data[i].name);
-                    A.appendChild(textNode);
-                    A.href = data[i].url;
-                    DIV.appendChild(A);
-                    var p = document.createElement("P");
-                    // Change this to correspond 
-                    var amount = users[i].length;
-                    if (amount == undefined) {
-                        amount = 0;
+
+        function rsvp(test) {
+            ajaxRequest('GET', apiUrl + "/api/addRSVP/?user=" + user + "&id=" + test, function(data) {});
+        }
+
+        function remove(test) {
+            ajaxRequest('GET', apiUrl + "/api/removeRSVP/?user=" + user + "&id=" + test, function(data) {});
+        }
+        var searchTerm = document.getElementById("searchBar");
+        var buttonToSubmit = document.getElementById("findStores");
+        $('#findStores').submit(function(e) {
+            e.preventDefault();
+            var searchText = searchTerm.value;
+            var users = [];
+            var counter = 0;
+            ajaxRequest('GET', apiUrl + "api/yelp/?location=" + searchText, function(data) {
+                data = JSON.parse(data);
+
+                mySyncFunction(counter);
+
+                function mySyncFunction(counter) {
+                    if (counter === undefined) {
+                        counter = 0;
                     }
-                    var textNode = document.createTextNode(amount + " going");
-                    p.appendChild(textNode);
-                    DIV.appendChild(p);
-                    if (logged) {
-                        var button = document.createElement("BUTTON");
-                        if (users[i].indexOf(user) == -1) {
-                            button.onclick = function() {
-                                rsvp(this.id);
-                            };
-                            var textNode = document.createTextNode("attend");
-                        } else {
-                            button.onclick = function() {
-                                remove(this.id);
-                            };
-                            var textNode = document.createTextNode("remove");
+                    if (counter >= 5) {
+                        theRest();
+                        return;
+                    };
+                    ajaxRequest('GET', apiUrl + "api/getUsers/?id=" + data[counter].id, function(data) {
+                        data = JSON.parse(data);
+                        users.push(data);
+                        counter++;
+                        mySyncFunction(counter);
+                    })
+                }
+
+                function theRest() {
+
+                    for (var i = 0; i < 5; i++) {
+
+                        var amount = users[i].length;
+                        if (amount == undefined) {
+                            amount = 0;
                         }
-                        button.id = data[i].id;
-                        button.appendChild(textNode);
-                        DIV.appendChild(button);
+                        amount += " going";
+                        data[i].amountOfUsers = amount;
+                       
+                        if (logged) {
+                            if (users[i].indexOf(user) == -1) {
+         
+                              data[i].buttonText = "ATTEND";
+                            } else {
+
+                              data[i].buttonText = "REMOVE";
+                            }
+                          data[i].buttonID = data[i].id;
+                        }
+                        $scope.$apply(function() {
+                            $scope.stores.push(data[i]);
+                        });
+                      
+                      if (logged) {
+                            if (users[i].indexOf(user) == -1) {
+                         
+                             document.getElementById(data[i].id).addEventListener("click", function(){
+        rsvp(this.id);
+                    });
+                                 
+                      
+                       
+                            } else {
+                                       
+                               document.getElementById(data[i].id).addEventListener("click", function(){
+        remove(this.id);
+                    });
+                              
+                            }
+                         
+                        }
+               
                     }
-                    var p = document.createElement("P");
-                    // Change this to correspond 
-                    var textNode = document.createTextNode(data[i].rating);
-                    p.appendChild(textNode);
-                    DIV.appendChild(p);
-                    var p = document.createElement("P");
-                    // Change this to correspond 
-                    var textNode = document.createTextNode(data[i].price);
-                    p.appendChild(textNode);
-                    DIV.appendChild(p);
                 }
-            }
+            });
         });
     });
-  });
 })();
