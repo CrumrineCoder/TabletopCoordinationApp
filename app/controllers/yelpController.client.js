ef5f277206e72ad09cf375a282c9dfdda928268a
Glitch (tabletop) <none>
2018-01-16 15:55:27 +0000

    Comments added

--- app/controllers/yelpController.client.js
+++ app/controllers/yelpController.client.js
@@ -1,16 +1,17 @@
-
 'use strict';
 (function() {
+    // Angular module set up
     var app = angular.module('yelp', []);
+    // Since handlebars already uses {{ }}, I have to change angular to {[{ }]}
     app.config(function($interpolateProvider) {
         $interpolateProvider.startSymbol('{[{');
         $interpolateProvider.endSymbol('}]}');
     });
+    // Set up the controller
     app.controller('yelpController', function($scope) {
+        // This is where we store everything we need to display to the user
         $scope.stores = [];
-  
-
-
+        // Do something as soon as the page loads
         function ready(fn) {
             if (typeof fn !== 'function') {
                 return;
@@ -20,7 +21,7 @@
             }
             document.addEventListener('DOMContentLoaded', fn, false);
         }
-
+        // Make requests to the backend
         function ajaxRequest(method, url, callback) {
             var xmlhttp = new XMLHttpRequest();
             xmlhttp.onreadystatechange = function() {
@@ -33,17 +34,17 @@
         }
         var apiUrl = 'https://tabletop.glitch.me/';
         var user;
-
+        // Tell if the current user logged in is the same user as the data. 
         function getUser(callback) {
             ajaxRequest('GET', apiUrl + "users/user_data", function(data) {
                 data = JSON.parse(data);
-              
                 if (data.hasOwnProperty('username')) {
                     user = data.username;
                 }
                 callback();
             });
         }
+        // Tell if the user is logged in 
         var logged = false;
         getUser(function() {
             if (user != undefined) {
@@ -52,24 +53,32 @@
         });
         var searchTerm = document.getElementById("searchBar");
         var buttonToSubmit = document.getElementById("findStores");
+        // Event listener for the form submission
         $('#findStores').submit(function(e) {
-               $scope.stores = [];
+            // Reset the variable holding the stuff to display everytime. If this line wasn't here we would only be adding to stuff on screen without removing the past search results. 
+            $scope.stores = [];
+            // Stop the form from default submission
             e.preventDefault();
+            // Get the term the user searched for 
             var searchText = searchTerm.value;
             var users = [];
             var counter = 0;
+            // Get the locations for the Yelp API
             ajaxRequest('GET', apiUrl + "api/yelp/?location=" + searchText, function(data) {
                 data = JSON.parse(data);
-      
                 mySyncFunction(counter);
+
                 function mySyncFunction(counter) {
+                    // If the counter somehow becomes undefined
                     if (counter === undefined) {
                         counter = 0;
                     }
+                    // After getting all the data. We have to do this because we're calling the database each time. 
                     if (counter >= 6) {
                         theRest();
                         return;
                     };
+                    // Recursion. This function will get all of the users from a search text of a store. 
                     ajaxRequest('GET', apiUrl + "api/getUsers/?id=" + data[counter].id, function(data) {
                         data = JSON.parse(data);
                         users.push(data);
@@ -77,29 +86,24 @@
                         mySyncFunction(counter);
                     })
                 }
-
+                // Add a user to the store
                 function rsvp(id, number) {
                     $('#' + id).off().on('click', function() {
                         remove(this.id, number)
                     })
-                    
-                  $("#"+$scope.stores[number].buttonID).toggleClass( "remove" );
-             
+                    $("#" + $scope.stores[number].buttonID).toggleClass("remove");
                     $scope.$apply(function() {
                         $scope.stores[number].amountOfUsers += 1;
                         $scope.stores[number].buttonText = "REMOVE";
-                      //  $scope.stores[number].css("opacity", "0.6");
                     })
                     ajaxRequest('GET', apiUrl + "/api/addRSVP/?user=" + user + "&id=" + id, function(data) {});
                 }
-
+                // Remove a user from the store
                 function remove(id, number) {
                     $('#' + id).off('click').on('click', function() {
                         rsvp(this.id, number)
                     })
-                    $("#"+$scope.stores[number].buttonID).toggleClass( "remove" );
-   
-              
+                    $("#" + $scope.stores[number].buttonID).toggleClass("remove");
                     $scope.$apply(function() {
                         $scope.stores[number].amountOfUsers -= 1;
                         $scope.stores[number].buttonText = "ATTEND";
@@ -109,32 +113,35 @@
 
                 function theRest() {
                     for (var i = 0; i < 6; i++) {
+                        // Get the amount of users 
                         var amount = users[i].length;
                         if (amount == undefined) {
                             amount = 0;
                         }
                         data[i].amountOfUsers = amount;
+                        // If the user is logged in then they can attend and so add the buttons. 
                         if (logged) {
                             if (users[i].indexOf(user) == -1) {
                                 data[i].buttonText = "ATTEND";
                             } else {
                                 data[i].buttonText = "REMOVE";
-                        
                             }
                             data[i].buttonID = data[i].id;
                         }
+                        // Show the stores to the user
                         $scope.$apply(function() {
                             $scope.stores.push(data[i]);
                         });
                         (function() {
                             var numb = i;
                             if (logged) {
+                                // If the user logged in is not the same as the group attending already, add an "Attend" button. If not, add a "Remove" button. 
                                 if (users[i].indexOf(user) == -1) {
                                     $("#" + data[i].id).on("click", function() {
                                         rsvp(this.id, numb);
                                     });
                                 } else {
-                                    $("#"+data[i].id).toggleClass( "remove" );
+                                    $("#" + data[i].id).toggleClass("remove");
                                     $("#" + data[i].id).on("click", function() {
                                         remove(this.id, numb);
                                     });
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 'use strict';
 /* Authentication: */
 /*
@@ -13,14 +12,13 @@ var flash = require('connect-flash');
 var session = require('express-session');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-
-
+// Connect to the database with mongoose
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://' + process.env.HOST + '/' + process.env.NAME, {
     useMongoClient: true
 });
 var db = mongoose.connection;
-//var polls = require('./app/routes/polls.js');
+// Route and set up Express
 var users = require('./app/routes/users.js');
 var express = require('express'),
     routes = require('./app/routes/index.js'),
@@ -28,40 +26,36 @@ var express = require('express'),
 var MongoClient = mongodb.MongoClient;
 var mLab = 'mongodb://' + process.env.HOST + '/' + process.env.NAME;
 var app = express();
-
-// Taken from: c
+// Set up handlebars, which is the view engine for html
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs({
     defaultLayout: 'layout'
 }));
 app.set('view engine', 'handlebars');
-
+// Set up body and cookie parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(cookieParser());
-
+// Connect the JS, HTML, and CSS
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/views', express.static(process.cwd() + '/views'));
-
+// Set up the passport session
 app.use(session({
     secret: process.env.PASSKEY,
     saveUninitialized: true,
     resave: true
 }));
-
 app.use(passport.initialize());
 app.use(passport.session());
-//app.use(passport.authenticate('remember-me'));
-
+// Error message formatting
 app.use(expressValidator({
     errorFormatter: function(param, msg, value) {
         var namespace = param.split('.'),
             root = namespace.shift(),
             formParam = root;
-
         while (namespace.length) {
             formParam += '[' + namespace.shift() + ']';
         }
@@ -72,9 +66,9 @@ app.use(expressValidator({
         };
     }
 }));
-
+// Used to show the error and success messages
 app.use(flash());
-
+// Set up the req.user variable and the msgs
 app.use(function(req, res, next) {
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
@@ -82,22 +76,18 @@ app.use(function(req, res, next) {
     res.locals.user = req.user || null;
     next();
 });
-
-//app.use('/', routes);
+// /users will route to the users routes
 app.use('/users', users);
-
-
+// Connect to MongoDB and route it to the routing
 MongoClient.connect(mLab, function(err, db) {
- 
     if (err) {
         throw new Error('Database failed to connect!');
     } else {
-      //  console.log('MongoDB successfully connected on port 27017.');
+        //  console.log('MongoDB successfully connected on port 27017.');
     }
- 
     //Exports the routes to app and db
     routes(app, db);
     app.listen(3000, function() {
-    //    console.log('Listening on port 3000...');
+        //    console.log('Listening on port 3000...');
     });
 });
